47b52d69622ccbd2a7f0be6a3059b33bbebeae31
EC2 Default User <ec2-user@ip-172-31-67-151.ec2.internal>
2021-11-08 09:39:44 +0800

    add

--- market_data_reminder.py
+++ market_data_reminder.py
@@ -8,7 +8,12 @@ body = { "tag": "text",
         "text": {"content": "Dearest Nat, could i trouble you for the rates today, it will be of great help in my quest! FX Rates in by 3.55pm, the rest by 6.30pm -Sent from AWS ",
                 "mentioned_email_list": ['natalie.huang@seamoney.com']}
         }
+        
+body2 = { "tag": "text",
+        "text": {"content": "Dearest Nat, could i trouble you for the rates today, it will be of great help in my quest! FX Rates in by 3.55pm -Sent from AWS ",
+                "mentioned_email_list": ['natalie.huang@seamoney.com']}
+        }
 
-data = json.dumps(body)
+data = json.dumps(body2)
 
 response = requests.post('https://openapi.seatalk.io/webhook/group/YimMnwMARf6LBU6U-HKtWA', headers=headers, data=data)
--- test.py
+++ test.py
@@ -5,9 +5,9 @@ headers = {
 }
 
 body = { "tag": "text",
-        "text": {"content": "Yay, will take a break now. See you all again on 8 Nov"}
+        "text": {"content": "Hi all, today is 8 Nov, will start work again. Daily reminders will be sent at 14:50 and 18:00. As as UAT 1"}
         }
-
+        
 data = json.dumps(body)
 
 response = requests.post('https://openapi.seatalk.io/webhook/group/YimMnwMARf6LBU6U-HKtWA', headers=headers, data=data)
