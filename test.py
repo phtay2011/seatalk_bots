58e7e18ad22a98da0f71d236e4332d107a968397
paul tay <paul.tay@shopee.com>
2022-03-02 16:18:19 +0800

    add

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ headers = {
 }
 
 body = { "tag": "text",
-        "text": {"content": "Hi all, today is 8 Nov, will start work again. Daily reminders will be sent at 14:50 and 18:00. As as UAT 1"}
+        "text": {"content": "Test"}
         }
-        
+
 data = json.dumps(body)
 
-response = requests.post('https://openapi.seatalk.io/webhook/group/YimMnwMARf6LBU6U-HKtWA', headers=headers, data=data)
+response = requests.post('https://openapi.seatalk.io/webhook/group/JKbYxfWXRj2PlbqWCwJ9Jw', headers=headers, data=data)
