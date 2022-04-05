9d680ede5b009155d1623a3b498ed41aca8f28f1
Woon Tian Li <tianli.woon@seamoney.com>
2022-04-05 12:16:18 +0800

    no message

--- market_data_reminder.py
+++ market_data_reminder.py
@@ -5,16 +5,16 @@ headers = {
 }
 
 body = { "tag": "text",
-        "text": {"content": "Dearest Nat, could i trouble you for the rates today, it will be of great help in my quest! FX Rates in by 3.55pm, the rest by 6.30pm -Sent from AWS ",
-                "mentioned_email_list": ['natalie.huang@seamoney.com']}
+        "text": {"content": "Hi Nat/Kah Lun, kindly refresh the rate files for today. FX Rates in by 3.55pm, the rest by 6.30pm -Sent from AWS ",
+                "mentioned_email_list": ['natalie.huang@seamoney.com'],['kahlun.leong@maribank.com.sg']}
         }
-        
+
 body2 = { "tag": "text",
         "text": {"content": "Dearest Nat, could i trouble you for the rates today, it will be of great help in my quest! FX Rates in by 3.55pm -Sent from AWS ",
                 "mentioned_email_list": ['natalie.huang@seamoney.com']}
         }
 
-        
+
 body3 = { "tag": "text",
         "text": {"content": "[TEST] Selamat pagi Jake! could i trouble you for the rates today. All Rates in by 6.30pm",
                 "mentioned_email_list": ['jake.wen@seamoney.com']}
@@ -22,7 +22,7 @@ body3 = { "tag": "text",
 
 data = json.dumps(body)
 
-response = requests.post('https://openapi.seatalk.io/webhook/group/YimMnwMARf6LBU6U-HKtWA', headers=headers, data=data)
+response = requests.post('https://openapi.seatalk.io/webhook/group/rtNj3dDVQG6SWLCSmOncGA', headers=headers, data=data)
 
 data3 = json.dumps(body3)
 #response3 = requests.post('https://openapi.seatalk.io/webhook/group/RxP3Cg97TRmIyzafYVW6KQ', headers=headers, data=data3)
